32ab9ac3128e8e045feec78b71f7a679dca39350
Ian Kabaka <iankabaka1@gmail.com>
2024-11-08 22:35:17 +0300

    adding data array to state

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -3,14 +3,6 @@ import React, { useEffect, useState } from "react";
 function PlantCard() {
   const [images, setImages] = useState("");
   const [names, setNames] = useState("");
-  useEffect(() => {
-    fetch("http://localhost:6001/plants")
-      .then((res) => res.json())
-      .then((data) => {
-        setImages(data.image);
-        setNames(data.name);
-      });
-  }, []);
   return (
     <li className="card" data-testid="plant-item">
       <img src={images} alt={"plant name"} />
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
+  const [data, setData] = useState([]);
+  console.log(data);
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+
   return <ul className="cards">{<PlantCard />}</ul>;
 }
 
