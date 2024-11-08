22e45a8618a2701b7f976fb2b3992a25b449c95b
Ian Kabaka <iankabaka1@gmail.com>
2024-11-08 22:30:42 +0300

    initial commit, no images shown

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function PlantCard() {
+  const [images, setImages] = useState("");
+  const [names, setNames] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.image);
+        setNames(data.name);
+      });
+  }, []);
   return (
     <li className="card" data-testid="plant-item">
-      <img src={"https://via.placeholder.com/400"} alt={"plant name"} />
-      <h4>{"plant name"}</h4>
+      <img src={images} alt={"plant name"} />
+      <h4>{names}</h4>
       <p>Price: {"plant price"}</p>
       {true ? (
         <button className="primary">In Stock</button>
--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -2,9 +2,7 @@ import React from "react";
 import PlantCard from "./PlantCard";
 
 function PlantList() {
-  return (
-    <ul className="cards">{/* render PlantCards components in here */}</ul>
-  );
+  return <ul className="cards">{<PlantCard />}</ul>;
 }
 
 export default PlantList;
