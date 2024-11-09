d7e1684a72bc1937614e9851f2002c8c261b7bcc
Ian Kabaka <iankabaka1@gmail.com>
2024-11-09 18:39:38 +0300

    binds the search input as a two way data binding

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+  const [search, setSearch] = useState("");
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
         type="text"
         id="search"
+        value={search}
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
+        onChange={(e) => setSearch(search)}
       />
     </div>
   );
