63a965e628e8bd28f3face872e2fc91d5b5fdb1a
Ian Kabaka <iankabaka1@gmail.com>
2024-11-09 23:10:32 +0300

    solves unique id issue when tests are run

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,10 +4,10 @@ import PlantCard from "./PlantCard";
 function PlantList({ data }) {
   return (
     <ul className="cards">
-      {data.map((item) => {
+      {data.map((item, index) => {
         return (
           <PlantCard
-            key={item.id}
+            key={item.id || index} //the test were getting undefined when no id was found, solves unique key error
             image={item.image}
             name={item.name}
             price={item.price}
