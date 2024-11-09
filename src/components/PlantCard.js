08dcc7d370db5dcd982fb7c3bff22c63419a0f18
Ian Kabaka <iankabaka1@gmail.com>
2024-11-09 18:36:29 +0300

    makes the button dynamic for marking a plant as sold out

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function PlantCard({ image, name, price }) {
+  const [sold, setSold] = useState(true);
+  function handleSold() {
+    setSold(!sold);
+  }
+
   return (
     <li className="card" data-testid="plant-item">
       <img src={image} alt={"plant name"} />
       <h4>{name}</h4>
       <p>Price: {price}</p>
-      {true ? (
-        <button className="primary">In Stock</button>
+      {sold ? (
+        <button onClick={handleSold} className="primary">
+          In Stock
+        </button>
       ) : (
-        <button>Out of Stock</button>
+        <button onClick={handleSold}>Out of Stock</button>
       )}
     </li>
   );
