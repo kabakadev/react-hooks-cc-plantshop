67fbe27f4df049d8a4216f9ffae66a9c29cf551e
Ian Kabaka <iankabaka1@gmail.com>
2024-11-09 11:15:42 +0300

    adding a submit handler to the form

--- src/components/NewPlantForm.js
+++ src/components/NewPlantForm.js
@@ -7,7 +7,7 @@ function NewPlantForm() {
   return (
     <div className="new-plant-form">
       <h2>New Plant</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="text" name="name" placeholder="Plant name" />
         <input type="text" name="image" placeholder="Image URL" />
         <input type="number" name="price" step="0.01" placeholder="Price" />
